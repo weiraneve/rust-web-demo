d793ebed1924424e652e9350ac4e9848497187e4
weiraneve <907221539@qq.com>
2023-10-19 19:53:05 +0800

    refactor: refactor simple api

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+struct AppState {
+    app_name: String,
+}
+
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+    format!("Hello {app_name}!")
 }
 
 #[post("/echo")]
@@ -18,7 +23,9 @@ async fn manual_hello() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
+            .app_data(web::Data::new(
+                AppState { app_name: String::from("Actix Web") }))
+            .service(index)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
